4ca3eabe165d9b41a5f83c25b86a88413b802ce0
Muskan-Mohammad <muskanmd2001@gmail.com>
2022-12-16 22:27:14 +0530

    new-update

--- src/apps/counter/counter.reducer.js
+++ src/apps/counter/counter.reducer.js
@@ -3,6 +3,7 @@ import { DECREMENT_ACTION , INCREMENT_ACTION } from "./counter.action";
 export function counterReducer(currentState = 100 , action){
     const payload = action.payload;
     const type = action.type;
+    console.log("counterReducer" , currentState , action)
 
     if (type === INCREMENT_ACTION.type){
         return currentState + 10;
